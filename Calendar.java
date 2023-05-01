554f80825a66147d71ab63bb36b5f2ec94cf4d00
Greyson Wilson <gqwilson@cpp.edu>
2023-05-01 16:52:45 -0700

    initial code

---
+++ Calendar.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Calendar {
+    ArrayList<Schedule> ScheduleList;
+    ArrayList<Integer>[][] monthAndDayList;
+    Display display;
+
+    public boolean createTask(Task task){
+
+    }
+
+    public boolean deleteTask(Task task){
+
+    }
+
+    public boolean searchTask(String name){
+
+    }
+
+    public void loadSchedule(int date){
+
+    }
+
+    public void getTasksForDay(int date){
+
+    }
+
+    public void getTasksForWeek(int date){
+
+    }
+
+    public void getTasksForMonth(int date){
+
+    }
+
+    public void getTasks(){
+
+    }
+
+    public Schedule returnScheduleFile(int date){
+
+    }
+
+    public boolean checkForConflict(Task task){
+
+    }
+}
