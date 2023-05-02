da1e7fdbbccf3f42c2045d19c350900899caed6f
Greyson Wilson <gqwilson@cpp.edu>
2023-05-01 17:13:35 -0700

    initial code

---
+++ src/ScheduleModel.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.util.ArrayList;
+
+public class ScheduleModel {
+    ArrayList<Schedule> scheduleList;
+
+    public TaskActivity createTask(String name){
+        
+    }
+
+    public void deleteTask(String name){
+
+    }
+
+    public TaskActivity editTask(String name){
+
+    }
+
+}
