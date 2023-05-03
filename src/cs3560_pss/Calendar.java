f44cb334cbbb5793ef4ff430f1ed37ef2f6403fe
Greyson Wilson <gqwilson@cpp.edu>
2023-05-02 18:43:41 -0700

    create task partial done

--- src/cs3560_pss/Calendar.java
+++ src/cs3560_pss/Calendar.java
@@ -1,4 +1,6 @@
 package cs3560_pss;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Calendar {
@@ -12,9 +14,42 @@ public class Calendar {
         display = new Display(null);
     }
 
-    public boolean createTask(TaskActivity task){
-		return false;
+    public boolean createTask(String name, int startTime, int endTime, int date, String typeString, int frequency, int startDate, int endDate){
+      if (typeString == "recurring"){
+        RecurringTaskActivity task = new RecurringTaskActivity(name, startTime, endTime, date, frequency, startDate, endDate);
+        
+        for (int i=startDate; i<endDate; i+=frequency){
+          Schedule schedule = scheduleList.get(i);
+          if (checkForConflict(task) == true){
+            schedule.addTask(task);
+          }
 
+          else {
+            //Revert tasks notify user
+            
+          }
+
+        }
+      }
+
+      else if (typeString == "transient"){
+        TransientTaskActivity task = new TransientTaskActivity(name, startTime, endTime, date);
+        Schedule schedule = scheduleList.get(date);
+        if (checkForConflict(task) == true){
+          schedule.addTask(task);
+        }
+        
+        else{
+          //error TODO --Conflict try a different time
+        }
+
+      }
+
+      else if (typeString == "anti"){
+
+      }
+
+      return false;
     }
 
     public boolean deleteTask(TaskActivity task){
