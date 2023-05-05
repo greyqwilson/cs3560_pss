4c45904bd50167818961310409c22fc0ca459024
Greyson Wilson <gqwilson@cpp.edu>
2023-05-04 17:51:55 -0700

    Begin implementation of displays

--- src/cs3560_pss/Display.java
+++ src/cs3560_pss/Display.java
@@ -1,21 +1,40 @@
 package cs3560_pss;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Display{
 
     private Calendar calendar;
+    private int currentDay, currentWeek, currentMonth;
+    final String SCHEDULE_DIR = ".\\schedules\\";
+    
 
     public Display(Calendar calendar){
         this.calendar = calendar;
     }
 
 
-    public TaskActivity searchTask(TaskActivity task){
-		return task;
-
+    public TaskActivity searchTask(String taskName){
+      TaskActivity task = calendar.searchTask(taskName);
+		  return task;
     }
 
-    public void loadScheduleFromFile(){
+    public void loadScheduleFromFile() {
+      Schedule schedFromFile;
+      File file = new File(SCHEDULE_DIR);
+      //Open file for reading
+      try (FileReader fw = new FileReader(file)){
+        
+      }
+      catch (IOException e){
 
+      }
+
+      //Copy relevant data to a schedule object
+      //Pass into whoever takes it and display
+      
     }
 
     public void writeScheduleToFile(){
@@ -23,12 +42,33 @@ public class Display{
     }
 
     public void displayTasksforDay(){
+      TaskActivity[] tasks;
+      tasks = calendar.getTasksForDay(currentDay);
+      printTaskList(tasks);
 
     }
     public void displayTasksforWeek(){
-        
+      Schedule[] weekSchedule;
+      weekSchedule = calendar.getTasksForWeek(currentWeek);
+
+      TaskActivity[] tasks;
+      for (int i=0; i<7; i++){
+        tasks[i] = calendar.getTasksForDay(i);
+        printTaskList(tasks);
+      }
     }
     public void displayTasksforMonth(){
+      Schedule[] monthSchedule = calendar.getTasksForMonth(currentMonth);
+      TaskActivity[] tasks;
+      for (int i=0; i<monthLength; i++){
+        tasks[i] = calendar.getTasksForDay(i);
+
+      }
+    }
+
+    public void printTaskList(TaskActivity[] taskList){
+      for (TaskActivity task : taskList) {
         
+      }
     }
 }
