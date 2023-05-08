1699ec5b953f4b63b428cd91e787dc3d8113368e
mievenfall <mngoc2603@gmail.com>
2023-05-07 23:42:12 -0700

    Update formatMonthTasks for the Monthyly view

--- src/cs3560_pss/Display.java
+++ src/cs3560_pss/Display.java
@@ -66,9 +66,7 @@ public class Display{
     }
     public void displayTasksforMonth(){
       Schedule[] monthSchedule = calendar.getTasksForMonth(currentMonth);
-      TaskActivity[] tasks;
-      for (int i=0; i<monthLength; i++){
-        tasks[i] = calendar.getTasksForDay(i);
+      formatMonthTasks(monthSchedule);
 
       }
     }
@@ -207,6 +205,98 @@ public class Display{
       
       
     }
+
+    private static String formatMonthTasks(Schedule[] schedules){
+
+      Schedule schedule;
+  
+      final int MAXTASKSLISTED = 20;
+      final int MAXTITLELENGTH = 8;
+      ArrayList< ArrayList< String > > monthData = new ArrayList< ArrayList< String > >();
+      
+      //Go through each schedule
+      StringBuilder monthHeadingSb = new StringBuilder("");
+      
+      //For use in formatting column width
+      int[] headingLength = new int[schedules.length];
+      
+      //Get the length of the schedule with the most amount of tasks
+      int knownMaxTasks = 0;
+      
+      for (int i = 0; i < schedules.length; i++){
+          //Record schedule's tasks
+          ArrayList< String > scheduleData = new ArrayList< String >();
+  
+          schedule = schedules[i];
+          ArrayList<TaskActivity> tasks = schedule.getTaskList();
+  
+          String day, month;
+          day = schedules[i].getDateString().substring(6, 8);
+          month = numberMonthToString(schedules[i].getDateString().substring(4, 6));
+          String heading = "*[" + day + " " + month + "]-----------------";
+          headingLength[i] = heading.length();
+          monthHeadingSb.append(heading);
+          
+         
+          if (tasks.size() > knownMaxTasks)
+              knownMaxTasks = tasks.size();
+  
+          //Grab all pertinent data from each day's task list (up to 20 tasks for any given day)
+          String startTime, endTime, title;
+          for (int j = 0; j < tasks.size() || j == MAXTASKSLISTED-1; j++){
+              title = tasks.get(j).getName();
+              //startTime = String.valueOf(tasks.get(j).getStartTime());
+              //endTime = String.valueOf(tasks.get(j).getEndTime());
+              startTime = numberTimeToString(tasks.get(j).getStartTime(), false);
+              endTime = numberTimeToString(tasks.get(j).getStartTime(), false);
+              
+              if (title.length() > MAXTITLELENGTH){
+                  title = title.substring(0, MAXTITLELENGTH).concat("..");
+              }
+              scheduleData.add(j, startTime + "-" + endTime + " " + title);
+          }
+          monthData.add(i, scheduleData);
+          
+      }
+      monthHeadingSb.append("*\n");
+  
+      // Loop to display remaining days in the month
+      for (int i = 0; i < schedules.length; i++) {
+          if (i >= knownMaxTasks) {
+              String day = schedules[i].getDateString().substring(6, 8);
+              String month = numberMonthToString(schedules[i].getDateString().substring(4, 6));
+              String heading = "*[" + day + " " + month + "]-----------------*";
+              headingLength[i] = heading.length();
+              monthHeadingSb.append(heading);
+              monthHeadingSb.append("|").append(" ".repeat(headingLength[i] - 3)).append("|").append("\n");
+              monthHeadingSb.append("*-----------------------*").append("\n");
+          }
+      }
+  
+      //Collate each line
+      for (int i = 0; i < MAXTASKSLISTED && i < knownMaxTasks; i++){
+          String taskLine;
+          //For each column
+          
+          for (int j = 0; j < monthData.size(); j++){
+              ArrayList< String > scheduleData = monthData.get(j);
+              if (i < scheduleData.size()){
+                  if (j == 0){
+                      monthHeadingSb.append("|");
+                  }
+                  taskLine = scheduleData.get(i);
+                  taskLine = taskLine.concat(" ".repeat(headingLength[i]-taskLine.length() -1) + "|");
+                  monthHeadingSb.append(taskLine);
+              }
+              else
+                  break;
+          }
+          monthHeadingSb.append("\n");
+      }
+  
+      return monthHeadingSb.toString();
+  
+  }
 
     private static String numberTimeToString(double time, boolean twentyFourHour){
       int hour = (int) time;
